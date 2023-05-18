6d0480467267e62beb5e1e13ab2c8f087de25786
Juan Guerra <juan.guerra@yale.edu>
2023-05-18 15:32:10 -0400

    packaging update

--- setup.py
+++ setup.py
@@ -3,6 +3,5 @@ from setuptools import find_packages, setup
 setup(
     name="dynamicAll",
     packages= ["dynamicAll"],
-    package_data = ['src/data'],
-    package_dir={"": "dynamicAll"},
+    package_dir={"": "src/"},
 )
