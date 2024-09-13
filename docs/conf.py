44dbf2c370febdb9f41e42a34dced3300a6ace01
Juan Guerra <juan.guerra@yale.edu>
2024-09-13 14:09:35 -0400

    more docs updates

--- docs/conf.py
+++ docs/conf.py
@@ -4,39 +4,35 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 
-
 import sys, os
-sys.path.insert(0, os.path.abspath('..'))
+
+sys.path.insert(0, os.path.abspath(".."))
 # sys.path.append(os.path.abspath('sphinxext'))
-
-
-
-
-
-
 
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'dynamicAll'
-copyright = '2023, Juan Guerra'
-author = 'Juan Guerra'
-release = '00.00.01'
+project = "dynamicAll"
+copyright = "2023, Juan Guerra"
+author = "Juan Guerra"
+release = "00.00.01"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-mathjax_path = "https://cdnjs.cloudflare.com/ajax/libs/mathjax/3.2.0/es5/tex-mml-chtml.min.js"
+mathjax_path = (
+    "https://cdnjs.cloudflare.com/ajax/libs/mathjax/3.2.0/es5/tex-mml-chtml.min.js"
+)
 
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon',
-    'nbsphinx',
-    'sphinx.ext.mathjax',
-    'myst_nb',  # myst_nb is a plugin for MyST Markdown that adds support for executing and displaying Jupyter notebooks.
-    'sphinx_copybutton',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.napoleon",
+    "nbsphinx",
+    "sphinx.ext.mathjax",
+    "myst_nb",  # myst_nb is a plugin for MyST Markdown that adds support for executing and displaying Jupyter notebooks.
+    "sphinx_copybutton",
     # "sphinx_new_tab_link",
 ]
 nbsphinx_execute = "off"
@@ -49,16 +45,31 @@ myst_enable_extensions = [
 ]
 myst_url_schemes = ("http", "https", "mailto")
 mathjax3_config = {
-    'tex': {'tags': 'ams', 'useLabelIds': True},
+    "tex": {"tags": "ams", "useLabelIds": True},
 }
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "pydata_sphinx_theme"
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 copybutton_prompt_text = r">>> |\.\.\. |\$ |In \[\d*\]: | {2,5}\.\.\.: | {5,8}: "
 copybutton_prompt_is_regexp = True
+
+html_theme_options = {
+    "icon_links": [
+        {
+            # Label for this link
+            "name": "GitHub",
+            # URL where the link will redirect
+            "url": "https://github.com/jguerra-astro/dynamicall",  # required
+            # Icon class (if "type": "fontawesome"), or path to local image (if "type": "local")
+            "icon": "fa-brands fa-square-github",
+            # The type of image to be used (see below for details)
+            "type": "fontawesome",
+        }
+    ]
+}
